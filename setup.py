5545faa89508bbb301e79a5f29ce2df223843d82
bhalla02 <sarthakbhalla723@gmail.com>
2024-04-28 18:36:55 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_reuirement(file_path:str)->List[str]:
+    requirement=[]
+    with open(file_path) as file_obj:
+       requirement= file_obj.readlines()
+       requirement =[req.replace("\n"," ")for req in requirement]
+
+       if HYPEN_E_DOT in requirement:
+           requirement.remove(HYPEN_E_DOT)
+
+       return requirement
+
+
+
+setup(
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='sarthak',
+    author_email='sarthakbhalla723@gmail.com',
+    install_requires=['pandas', 'numpy'],
+    packages=find_packages()
+)
+    
+                 
+                      
